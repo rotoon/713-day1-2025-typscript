dc836e960013c58b846410a617a84f375d54e7d0
rotoon <rotoon.np@gmail.com>
2025-11-29 16:32:54 +0700

    task 8 ข้อ 9

--- src/task8.ts
+++ src/task8.ts
@@ -1,7 +1,18 @@
-let x: number = 1
-let y: number = 2
-if (x > 5 && y > 12) {
-  console.log('ค่ามากจัง')
+// let x: number = 1
+// let y: number = 2
+// if (x > 5 && y > 12) {
+//   console.log('ค่ามากจัง')
+// } else {
+//   console.log(x * y + x / y)
+// }
+
+let x = 90
+if (x > 80) {
+  console.log('Grade A')
+} else if (x > 60) {
+  console.log('Grade B')
+} else if (x > 50) {
+  console.log('Grade C')
 } else {
-  console.log(x * y + x / y)
+  console.log('Grade F')
 }
